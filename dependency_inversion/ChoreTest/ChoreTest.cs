91c183b3410aefc1650beb433313dfb1aa969069
Alexander Nitsch <alexander.nitsch@scs.ch>
2023-03-24 14:04:03 +0000

    feat(ChoreCLI): add test project

---
+++ dependency_inversion/ChoreTest/ChoreTest.cs
@@ -0,0 +1,19 @@
+﻿namespace ChoreTest;
+
+using NUnit.Framework;
+using ChoreLibrary;
+
+public class Tests
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void FakeTest()
+    {
+        Assert.AreEqual(0, 0);
+    }
+
+}
