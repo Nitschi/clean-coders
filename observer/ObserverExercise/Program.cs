0f5d00bcba6e46d635fa86d1f26dc7ec8c5589ae
Alexander Nitsch <alex@nits.ch>
2023-07-06 11:59:17 +0000

    refactor(observer): move project one folder up

--- observer/ObserverExercise/Program.cs
+++
@@ -1,3 +0,0 @@
-var alarm = new Alarm();
-
-alarm.TriggerAlarm();
---
+++ observer/Program.cs
@@ -0,0 +1,5 @@
+// Based the exercise from https://github.com/raw-coding-youtube/design-patterns.
+
+var alarm = new Alarm();
+
+alarm.TriggerAlarm();
